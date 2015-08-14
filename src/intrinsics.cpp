9fd6d81123c5044a22a8156c86cd36f65adca899
emiswelt <emiswelt@gmail.com>
2015-08-14 18:38:40 +0200

    added intrinsics file.

---
+++ src/intrinsics.cpp
@@ -0,0 +1,6 @@
+#include <opencv2/opencv.hpp>
+
+namespace optonaut {
+    double iPhone6IntrinsicsData[9] = {4.854369, 0, 3, 0, 4.854369, 2.4, 0, 0, 1};
+    cv::Mat iPhone6Intrinsics(3, 3, CV_64F, iPhone6IntrinsicsData);
+}
