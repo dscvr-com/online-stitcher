e51f2900881a13489f096f606e5a95b228d9332a
emiswelt <emiswelt@gmail.com>
2015-08-20 17:10:26 +0100

    Added data ref to image, so the image can be lazily loaded

--- src/image.hpp
+++ src/image.hpp
@@ -7,8 +7,24 @@
 #define OPTONAUT_IMAGE_HEADER
 
 namespace optonaut {
+
+    namespace colorspace {
+        const int RGBA = 0;
+        const int RGB = 1;
+    }
+
+    struct ImageRef {
+        void* data;
+        int width;
+        int height;
+        int colorSpace;
+
+        ImageRef() : data(NULL), width(0), height(0), colorSpace(colorspace::RGBA) { }
+    }
+
 	struct Image {
 		cv::Mat img;
+        ImageRef dataRef;
 		cv::Mat extrinsics;
 		cv::Mat intrinsics; 
 		int id;
@@ -16,6 +32,25 @@ namespace optonaut {
 
 		std::vector<cv::KeyPoint> features;
 		cv::Mat descriptors;
+
+        Image()  : img(0, 0, CV_8UC3), extrinsics(4, 4, CV_64F), intrinsics(3, 3, CV_64F), source("Unknown")
+
+        void IsLoaded() {
+            return img.cols != 0 && img.rows != 0;
+        }
+
+        void LoadFromDataRef() {
+            assert(!IsLoaded);
+            assert(imageRef.data != NULL);
+            assert(imageRef.colorSpace = colorspace::ARGB); 
+
+            cvtColor(Mat(imageRef.height, imageRef.width, CV_8UC4, imageRef.data), img, COLOR_RGBA2RGB);
+        }
+
+        void Unload() {
+            img = Mat(0, 0, CV_8UC3);
+            //Todo: Is this enough to dispose the image?
+        }
 	};
 
 	typedef std::shared_ptr<Image> ImageP;
