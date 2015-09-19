003af3b8a3c0ec309f71e5b4422e841e22db3811
emiswelt <emiswelt@gmail.com>
2015-09-19 11:37:11 +0100

    Added bundle adjuster

---
+++ src/bundleAligner.hpp
@@ -0,0 +1,55 @@
+#include <opencv2/features2d.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
+#include <deque>
+
+#include "image.hpp"
+#include "support.hpp"
+#include "aligner.hpp"
+#include "pairwiseVisualAligner.hpp"
+#include "stat.hpp"
+
+using namespace cv;
+using namespace std;
+
+#ifndef OPTONAUT_BUNDLE_ALIGNMENT_HEADER
+#define OPTONAUT_BUNDLE_ALIGNMENT_HEADER
+
+namespace optonaut {
+	class BundleAligner : public Aligner {
+	private:
+		PairwiseVisualAligner visual;
+        vector<ImageP> images;
+    public: 
+		BundleAligner() : visual(PairwiseVisualAligner::ModeFeatures) { }
+
+        bool NeedsImageData() {
+            return true;
+        }
+
+        void Dispose() {
+
+        }
+
+		void Push(ImageP next) {
+            for(auto img : images) {
+                if(GetAngleOfRotation(img->originalExtrinsics, next->originalExtrinsics) < M_PI / 2) {
+                //if(visual.AreOverlapping(next, img)) {
+                     visual.FindCorrespondence(next, img);
+                     visual.FindCorrespondence(img, next);
+                }
+            } 
+            images.push_back(next);
+        }
+
+		Mat GetCurrentRotation() const {
+			return images[images.size() - 1]->originalExtrinsics;
+		}
+
+        void Postprocess(vector<ImageP> imgs) const {
+            visual.RunBundleAdjustment(imgs);
+        };
+	};
+}
+
+#endif
