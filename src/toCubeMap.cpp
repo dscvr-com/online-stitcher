8ca96a56dff9c97a110d57baf3ae0ff02eac048f
emiswelt <emiswelt@gmail.com>
2016-01-11 12:04:20 +0000

    Added sub-faces to cubemap.

--- src/toCubeMap.cpp
+++ src/toCubeMap.cpp
@@ -11,7 +11,7 @@ using namespace cv;
 
 void printUsage() {
    cout << "Simple equiretangular optograph to cubemap converter." << endl;
-   cout << "usage: to-cube-map [INPUT-IMAGE] [OUTPUT-IMAGE] [FACE-ID]" << endl; 
+   cout << "usage: to-cube-map [INPUT-IMAGE] [OUTPUT-IMAGE] [FACE-ID] [SUB X] [SUB Y] [SUB WIDTH] [SUB HEIGHT]" << endl; 
 }
 
 float faceTransform[6][2] = 
@@ -25,7 +25,16 @@ float faceTransform[6][2] =
     };
 
 
-inline void createCubeMapFace(const Mat &optograph, Mat &face, int faceId = 0, const int width = -1, const int height = -1) {
+inline void createCubeMapFace(const Mat &optograph, Mat &face, 
+        const int faceId, const int width, const int height, 
+        const float subX = 0, const float subY = 0, 
+        const float subW = 1, const float subH = 1) {
+
+    assert(faceId >= 0 && faceId < 6);
+    assert(subX >= 0 && subX + subW <= 1);
+    assert(subY >= 0 && subY + subH <= 1);
+    assert(subW > 0);
+    assert(subH > 0);
 
     Mat in = optograph;
     float inWidth;
@@ -59,9 +68,20 @@ inline void createCubeMapFace(const Mat &optograph, Mat &face, int faceId = 0, c
     for(int y = 0; y < height; y++) {
         for(int x = 0; x < width; x++) {
 
-            // Map texture to unit space
-            float nx = (float)y / (float)height - 0.5f;
-            float ny = (float)x / (float)width - 0.5f;
+            // Map texture to unit space [0, 1]
+            float nx = (float)y / (float)height;
+            float ny = (float)x / (float)width;
+
+            // Subface
+            nx *= subW;
+            ny *= subH;
+
+            nx += subX;
+            ny += subY;
+
+            // Remap to [-an, an]
+            nx -= 0.5f;
+            ny -= 0.5f;
 
             nx *= 2 * an; 
             ny *= 2 * an; 
@@ -140,16 +160,28 @@ int main(int argc, char** argv) {
 
     const int w = 1024;
    
-    if(argc == 4) { 
+    if(argc >= 4) { 
         int faceId = std::stoi(string(argv[3]));
+
+        float subX = 0;
+        float subY = 0;
+        float subW = 1;
+        float subH = 1;
 
         if(faceId < 0 || faceId > 5) {
             cout << "Faceid has to be between 0 and 5, inclusive." << endl;
             return 1;
         }
 
+        if(argc == 8) {
+            subX = std::atof(argv[4]);
+            subY = std::atof(argv[5]);
+            subW = std::atof(argv[6]);
+            subH = std::atof(argv[7]);
+        }
+
         Mat face;
-        createCubeMapFace(in, face, faceId, w, w);
+        createCubeMapFace(in, face, faceId, w, w, subX, subY, subW, subH);
         imwrite(argv[2], face);
     } else {
         Mat target = Mat::zeros(w * 3, w * 4, CV_8UC3); 
