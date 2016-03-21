538ee46e7be38382bbaa14c9f1f9a2550a8c9cd5
emiswelt <emiswelt@gmail.com>
2016-03-21 20:46:54 +0800

    Documentation.

--- src/recorder/aligner.hpp
+++ src/recorder/aligner.hpp
@@ -9,24 +9,67 @@
 #define OPTONAUT_ALIGNMENT_HEADER
 
 namespace optonaut {
+    /*
+     * Encapsulates a keyframe (e.g. an image with known position) and the distance to that keyframe.
+     */
     struct KeyframeInfo {
         InputImageP keyframe;
         double dist;
     };
+
+    /*
+     * Aligner interface. Designed as a class that follows an asynchronous/streaming pattern. 
+     */
     class Aligner {
     public:
+
+        /*
+         * Sends an input image to this aligner. 
+         */
 		virtual void Push(InputImageP next) = 0;
+
+        /*
+         * Returns the current sensor bias, that is the difference between 
+         * the value received from the device sensors and the actual value. 
+         */
 		virtual cv::Mat GetCurrentBias() const = 0;
+
+        /*
+         * Frees all resources allocated by the aligner. 
+         */
         virtual void Dispose() = 0; 
+
+        /*
+         * True if image data has to be available for the next image, false if not. 
+         */
         virtual bool NeedsImageData() = 0;
+
+        /*
+         * Runs postprocessing steps on all given images. This has to be called when recording is finished. 
+         */
         virtual void Postprocess(std::vector<InputImageP> imgs) const = 0;
+
+        /*
+         * Finishes all asynchronous operations without releasing memory resources. 
+         */
         virtual void Finish() = 0;
+
+        /*
+         * Manually adds a keyframe (an image with a known position) to the aligner. 
+         */
         virtual void AddKeyframe(InputImageP next) = 0;
+
+        /*
+         * Gets the keyframes which are closest to the rotation given by search. 
+         */
         virtual std::vector<KeyframeInfo> GetClosestKeyframes(const cv::Mat &search, size_t count) const = 0;
         virtual double GetCurrentVtag() const {
             return 0;
         }
         
+        /* 
+         * Gets the keyframe closest do the rotation given by search. 
+         */
         InputImageP GetClosestKeyframe(const cv::Mat &search) const {
             auto keyframes = GetClosestKeyframes(search, 1);
             if(keyframes.size() != 1) {
--- src/recorder/alignmentGraph.hpp
+++ src/recorder/alignmentGraph.hpp
@@ -15,6 +15,10 @@ using namespace std;
 #define OPTONAUT_ALIGNMENT_GRAPH_HEADER
 
 namespace optonaut {
+
+    /*
+     * Represents a horizontal alignment difference between two images. 
+     */
     struct AlignmentDiff {
         double dphi; //Difference for rotating around the vertical axis
         double valid; //Is it valid?
@@ -39,6 +43,15 @@ namespace optonaut {
     };
     
 
+    /*
+     * Calculates and holds a set of alignment differences for a set of images.
+     * 
+     * The alignment differences are hold in a big graph. The graph can be converted to a linear
+     * equation system, which, when solved for the minimal error, provides an optimal solution for 
+     * minimizing the alignment errors. 
+     *
+     * It is also possible to insert artificial adges into the graph, for example for damping. 
+     */
     class AlignmentGraph: public ImageCorrespondenceGraph<AlignmentDiff> {
         private: 
             PairwiseCorrelator aligner;
@@ -47,27 +60,44 @@ namespace optonaut {
             static const bool debug = false;
             map<size_t, double> alignmentCorrections;
         public:
+
+            /*
+             * Creates a new alignment graph based on the given recorder graph. 
+             */
             AlignmentGraph(const RecorderGraph &graph, 
                     const BiMap<size_t, uint32_t> &imagesToTargets) : 
                 graph(graph), 
                 imagesToTargets(imagesToTargets)
             { }
+
+            /*
+             * Copy constructor. 
+             */
             AlignmentGraph(AlignmentGraph &ref) : 
                 graph(ref.graph), 
                 imagesToTargets(ref.imagesToTargets)
             {
                 SetAlignment(ref.GetAlignment());
             }
-        
+       
+            /*
+             * Sets given alignment offsets.
+             */ 
             void SetAlignment(map<size_t, double> alignment)
             {
                 this->alignmentCorrections = alignment;
             }
 
+            /*
+             * Gets the alignment offsets associated with this graph. 
+             */
             const map<size_t, double>& GetAlignment() const {
                 return alignmentCorrections;
             }
-        
+       
+            /*
+             * Calculates the alignment difference for two given images. 
+             */ 
             virtual AlignmentDiff GetCorrespondence(InputImageP imgA, InputImageP imgB, AlignmentDiff &aToB, AlignmentDiff &bToA) {
 
                 const bool dampUncorrelatedNeighbors = true;
@@ -82,6 +112,7 @@ namespace optonaut {
                 Assert(graph.GetPointById(pidA, tA));
                 Assert(graph.GetPointById(pidB, tB));
 
+                // Dist: Distance, in steps, between the selection points. 
                 int dist = min(abs((int)tA.localId - (int)tB.localId),
                        (int)tA.ringSize - abs((int)tA.localId - (int)tB.localId));
 
@@ -99,12 +130,14 @@ namespace optonaut {
                 double maxDist = dist * (M_PI * 2 / tA.ringSize);
                 double angularDiff = maxDist - angularDist; 
 
+                // If these are neighbors or we are on different rings, do try to match the images. 
                 if(tA.ringId != tB.ringId 
                         || areNeighbors) {
                     int minSize = min(imgA->image.cols, imgA->image.rows) / 1.8;
 
                     auto res = aligner.Match(imgB, imgA, minSize, minSize);
 
+                    // If our match was invalid, just do damping it. 
                     if(!res.valid && areNeighbors && dampUncorrelatedNeighbors) {
                         res.valid = true;
                         res.angularOffset.y = 0;
@@ -113,6 +146,7 @@ namespace optonaut {
                         aToB.forced = true;
                     }
 
+                    // Copy matching information to the correspondence info
                     aToB.dphi = res.angularOffset.y;
                     aToB.overlap = res.correlationCoefficient;
                     aToB.valid = res.valid;
@@ -123,12 +157,13 @@ namespace optonaut {
                     aToB.overlap = 0;
                     aToB.valid = false;
                 }
-                    
+                   
+                // Copy correspondence info for b<>a from a<>b since it is guaranteed to be symmetrical.  
                 bToA = aToB;
                 aToB.dphi *= -1;
 
+                // If configured so, add damping to neighors.  
                 if(dampAllNeighbors && areNeighbors) {
-
                     AssertM(false, "All neighbor damping implementation possibly incorrect - dphi is relative, also we should only damp successively recorded images");
 
                     AlignmentDiff aToBDamp, bToADamp; 
@@ -139,21 +174,28 @@ namespace optonaut {
                     aToBDamp.valid = true;
                     bToADamp = aToBDamp;
                     bToADamp.dphi *= -1;
-
+                       
+                    // Insert this extra correspondence.  
                     InsertCorrespondence(imgA->id, imgB->id, aToBDamp, bToADamp);
                 }
-                
+               
                 return aToB;
             };
 
+            /*
+             * From the alignment graph, finds a solution for re-aligning all images with minimal error. 
+             */
             Edges FindAlignment(double &outError) {
+
+                // Pre-calculate the size of our equation system. 
                 size_t maxId = 0;
                 Edges allEdges;
 
                 for(auto &adj : relations.GetEdges()) {
                     maxId = max(maxId, adj.first);
                 }
-                
+               
+                // Build forward/backward lookup tables.  
                 vector<int> remap(maxId);
                 vector<int> invmap;
                 
@@ -165,97 +207,105 @@ namespace optonaut {
                 int n = (int)invmap.size();
 
                 Mat res = Mat::zeros(n, 1, CV_64F);
-                
-                for(int i = 0; i < 1; i++) { 
-                    //Build equation systen
-                    //R = sum of relative offsets.
-                    //O = matrix of observation weights.
-                    //x = result, optimal relative offset. 
-                    Mat R, O, x;
-                    R = Mat::zeros(n, 1, CV_64F);
-                    O = Mat::zeros(n, n, CV_64F);
-                    x = Mat::zeros(n, 1, CV_64F);
-
-                    //Alpha - damping for our regression. 
-                    double alpha = 2;
-                    double beta = 1 / alpha;
-                    double error = 0;
-                    double weightSum = 0;
-                    double edgeCount = 0;
-
-                    const double quartil = 0.1;
-
-                    for(auto &adj : relations.GetEdges()) {
-
-                        Edges correlatorEdges = fun::filter<Edge>(adj.second, 
-                            [] (const Edge &a) {
-                                return !a.value.forced && a.value.overlap > 0
-                                && a.value.valid; 
-                            }); 
-
-                        Edges forcedEdges = fun::filter<Edge>(adj.second, 
-                            [] (const Edge & a) {
-                                return a.value.forced && a.value.overlap > 0 
-                                && a.value.valid; 
-                            }); 
-
-                        //Reject upper quartil only and sort by absolute delta. 
-                        std::sort(correlatorEdges.begin(), correlatorEdges.end(), 
-                            [] (const Edge &a, const Edge &b) {
-                                return abs(a.value.dphi) < abs(b.value.dphi);
-                            });
-
-                        size_t m = correlatorEdges.size();
-
-                        for(size_t i = 0; i < m; i++) {
-                            if(i >= (double)m * (1.0 - quartil)) {
-                                correlatorEdges[i].value.quartil = true;
-                                allEdges.push_back(correlatorEdges[i]);
-                            } else {
-                                forcedEdges.push_back(correlatorEdges[i]);
-                            }
-                        }
-                        
-                        for(auto &edge : forcedEdges) {
-                            //Use non-linear weights - less penalty for less overlap. 
-                            assert(!edge.value.quartil);
-                            if(edge.value.overlap > 0) {
-                                double weight = edge.value.overlap;
-
-                                O.at<double>(remap[edge.from], remap[edge.to]) += 
-                                    beta * weight;
-                                O.at<double>(remap[edge.from], remap[edge.from]) += 
-                                    alpha * weight;
-                                R.at<double>(remap[edge.from]) += 
-                                    2 * weight * edge.value.dphi;
-
-                                if(!edge.value.forced) {
-                                    error += weight * abs(edge.value.dphi);
-                                    weightSum += weight;
-                                    edgeCount++;
-                                }
-
-                                allEdges.push_back(edge);
-                            }
-                        }
-                    }
-
-                    outError = error / relations.GetEdges().size();
                
-                    solve(O, R, x, DECOMP_SVD);
+                // Build equation systen
+                // R = sum of relative offsets.
+                // O = matrix of observation weights.
+                // x = result, optimal relative offset. 
+                Mat R, O, x;
+                R = Mat::zeros(n, 1, CV_64F);
+                O = Mat::zeros(n, n, CV_64F);
+                x = Mat::zeros(n, 1, CV_64F);
 
-                    res = res + x;
+                // Alpha - damping for our regression. 
+                double alpha = 2;
+                double beta = 1 / alpha;
+                double error = 0;
+                double weightSum = 0;
+                double edgeCount = 0;
 
-                    for(auto &adj : relations.GetEdges()) {
-                       for(auto &edge : adj.second) {
-                            edge.value.dphi -= x.at<double>(remap[edge.from]);
-                            edge.value.dphi += x.at<double>(remap[edge.to]);
-                       } 
+                // Quartil for selecting edges that are used when optimizing. 
+                const double quartil = 0.1;
+
+                for(auto &adj : relations.GetEdges()) { // For adjacency list each node in our graph...
+                    // Find all edges created by the aligner. 
+                    Edges correlatorEdges = fun::filter<Edge>(adj.second, 
+                        [] (const Edge &a) {
+                            return !a.value.forced && a.value.overlap > 0
+                            && a.value.valid; 
+                        }); 
+
+                    // Find all adges manually created.  
+                    Edges forcedEdges = fun::filter<Edge>(adj.second, 
+                        [] (const Edge & a) {
+                            return a.value.forced && a.value.overlap > 0 
+                            && a.value.valid; 
+                        }); 
+
+                    //Reject upper quartil of correlator edges only and sort by absolute delta. 
+                    std::sort(correlatorEdges.begin(), correlatorEdges.end(), 
+                        [] (const Edge &a, const Edge &b) {
+                            return abs(a.value.dphi) < abs(b.value.dphi);
+                        });
+
+                    size_t m = correlatorEdges.size();
+
+                    for(size_t i = 0; i < m; i++) {
+                        if(i >= (double)m * (1.0 - quartil)) {
+                            correlatorEdges[i].value.quartil = true;
+                            allEdges.push_back(correlatorEdges[i]);
+                        } else {
+                            // If the edge is not in the quartil, add it to the forced edges.
+                            forcedEdges.push_back(correlatorEdges[i]);
+                        }
                     }
+                    
+                    // For all edges we decide to work with...
+                    for(auto &edge : forcedEdges) {
+
+                        Assert(!edge.value.quartil);
+
+                        if(edge.value.overlap > 0) {
+                            double weight = edge.value.overlap;
+
+                            // Add values to equation system. 
+                            O.at<double>(remap[edge.from], remap[edge.to]) += 
+                                beta * weight;
+                            O.at<double>(remap[edge.from], remap[edge.from]) += 
+                                alpha * weight;
+                            R.at<double>(remap[edge.from]) += 
+                                2 * weight * edge.value.dphi;
+
+                            //Use non-linear weights - less penalty for less overlap. 
+                            if(!edge.value.forced) {
+                                error += weight * abs(edge.value.dphi);
+                                weightSum += weight;
+                                edgeCount++;
+                            }
+
+                            allEdges.push_back(edge);
+                        }
+                    }
+                }
+
+                outError = error / relations.GetEdges().size();
+          
+                // Solve the equation system for minimal error.  
+                solve(O, R, x, DECOMP_SVD);
+
+                res = res + x;
+
+                // Extract solution from equation system. 
+                for(auto &adj : relations.GetEdges()) {
+                   for(auto &edge : adj.second) {
+                        edge.value.dphi -= x.at<double>(remap[edge.from]);
+                        edge.value.dphi += x.at<double>(remap[edge.to]);
+                   } 
                 }
                 
                 assert((int)invmap.size() == n);
 
+                // Remember the alignment corrections. 
                 for (int i = 0; i < n; ++i) {
                     this->alignmentCorrections[invmap[i]] = res.at<double>(i, 0);
                     //cout << invmap[i] << " alignment: " << x.at<double>(i, 0) << endl;
@@ -264,6 +314,9 @@ namespace optonaut {
                 return allEdges;
             }
 
+            /*
+             * Applies the calculated alignment correction to the submitted image. 
+             */
             void Apply(InputImageP in) const {
                 Mat bias(4, 4, CV_64F);
 
